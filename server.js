94ce1d5bc9f1db740a0f64d89630ba6ede3c4f07
Jhon_Kennedy_ <42581655+JhonKennedy18@users.noreply.github.com>
2021-04-05 15:32:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+///BOT///
 const Discord = require('discord.js');
+const MessageEmbed = require('discord.js');
 const client = new Discord.Client();
-const {Token} = require('./config.json')
+const fs = require("fs");
+
+if (!fs.existsSync('./config.json')) Token = process.env.Token;
+else Token = require("./config.json");
+
 
 let prefix = ":"
 
@@ -15,7 +29,13 @@ client.on('message', message => {
 		const command = args.shift().toLowerCase();
 		
 		if (command == ("ping")) {
-		message.channel.send('Pong.');
+		let embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .addFields(
+            { name: '🏓 Pong', value: `${Date.now() - message.createdTimestamp}ms` },
+            { name: 'Ping Api', value: `${Math.round(client.ws.ping)}ms` },
+        );
+    message.channel.send(embed);
 	} 
 	else if (command == ("beep")) {
 		message.channel.send('Boop.');
@@ -23,7 +43,7 @@ client.on('message', message => {
 	else if (command == "server") {
 		message.channel.send(`El Server se llama: ${message.guild.name}\nTiene un total de: ${message.guild.memberCount} miembros`);
 	}
-console.log(`${message.author.username}: ${message.content}\n`);
+console.log(`${message.author.username} ${message.content}\n`);
 });
 
 
